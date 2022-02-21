4349f64091e4e0e3fab9262aa933dde7183bd467
dusy93 <dusy93@naver.com>
2022-02-21 17:37:13 +0900

    [View] viewController 정리

--- MoveItMovie/ViewController.swift
+++ MoveItMovie/ViewController.swift
@@ -12,37 +12,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let yesterdayDate = Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()
-        
-        var parameter: RequestDailyBoxOffice = RequestDailyBoxOffice()
-        parameter.targetDt = CommonUtils.convertDateToString(date: yesterdayDate, format: "yyyyMMdd")
-        parameter.itemPerPage = "1"
-        
-        do {
-            let jsonEncoder = JSONEncoder()
-            let encodingParam = try jsonEncoder.encode(parameter)
-            let jsonParam = try JSONSerialization.jsonObject(with: encodingParam, options: .allowFragments)
-            
-            guard let param = jsonParam as? Parameters else {
-                print("parameter error")
-                return
-            }
-            
-            OpenApiManager.sharedInstance
-                .requestDailyBoxOfficeList(Params: param, ServiceMode: .Daily) { data in
-                    do {
-                        let dailyBoxOfficeData = try JSONDecoder().decode(ResponceDailyBoxOffice.self, from: data)
-                        print(dailyBoxOfficeData.boxOfficeResult.dailyBoxOfficeList)
-                    } catch {
-                        print(error)
-                    }
-                } FailError: { errorCode in
-                    print("error \(errorCode)")
-                }
-        } catch {
-            print("error")
-        }
     }
 }
 
